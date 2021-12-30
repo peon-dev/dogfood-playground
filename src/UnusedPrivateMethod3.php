c8b754b4d92d37d0b8b8aaf782fd267c98aa002e
Jan Mikeš <j.mikes@me.com>
2021-12-30 01:25:56 +0100

    Added dogfood: Unused Private Method 3 + 1 (testing baseline)

--- src/UnusedPrivateMethod3.php
+++ src/UnusedPrivateMethod3.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace PHPMateDogFood;
 
-final class UnusedPrivateMethod
+final class UnusedPrivateMethod3
 {
     public function someMethod(): void
     {
         $this->used();
     }
-
 
     private function used(): void {}
     private function neverUsed(): void {}
